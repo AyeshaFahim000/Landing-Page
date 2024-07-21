16b31002b8ade985e5ea38f951590e8f7c049975
Ayesha Fahim <ayesha.fahim02@gmail.com>
2024-07-21 12:45:04 -0700

    Done

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import Navbar from './components/Navbar';
+// import Ban from './components/bannr';
+// import Service from './components/services'
+
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import Services from "./Components/Services"
+
+
+
+function App() {
+  return (
+    <>
+    <div className='body-container'>
+      <Navbar/>
+      <Banner/>
+      
+      
+      </div>
+      
+     <Services/>
+
+
+      
+     
+
+      
+      
+    </>
+    
+  );
+}
+
+export default App;
